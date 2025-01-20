8cadbe419b8134de75e0be7d3424c214e305b973
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2025-01-20 16:47:14 +0530

    user registration controller done
    user registration controller done

---
+++ User.Management.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using User.Management.API.Models;
+using User.Management.API.Models.Authentication.SignUp;
+
+namespace User.Management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        public AuthenticationController(UserManager<IdentityUser>userManager,
+            RoleManager<IdentityRole> roleManager,IConfiguration configuration) 
+        { 
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterUser registerUser, string role)
+        {
+            //Check User Exist
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+
+            if (userExist != null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden,
+                    new Response { Status = "Error", Message = "User already exists!" });
+
+            }
+            //add the user in the database
+
+            IdentityUser user = new()
+            {
+                Email = registerUser.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = registerUser.Username
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            return result.Succeeded
+            ? StatusCode(StatusCodes.Status201Created,
+                    new Response { Status = "Success", Message = "User Created Successfully!" })
+           
+            : StatusCode(StatusCodes.Status500InternalServerError,
+                    new Response { Status = "Error", Message = "User Failed to Create" });
+            
+
+            //Assign Role
+
+        }
+        }
+    }
