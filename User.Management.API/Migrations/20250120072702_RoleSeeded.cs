d653d15ba04da741b2dc5a7fa2e62369d5e5545d
Chalana Shehara <73055643+Chalana-98@users.noreply.github.com>
2025-01-20 14:23:45 +0530

    project initiate
    project initiate

---
+++ User.Management.API/Migrations/20250120072702_RoleSeeded.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace User.Management.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoleSeeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "3b098b51-725c-491c-85c5-e937c8ad6865", "1", "Admin", "ADMIN" },
+                    { "83f8b40e-19e2-43b1-9d46-0dc0ac9205fb", "3", "HR", "HR" },
+                    { "cd7d6b74-0fd3-4fc0-9d5d-61d2154d622d", "2", "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3b098b51-725c-491c-85c5-e937c8ad6865");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "83f8b40e-19e2-43b1-9d46-0dc0ac9205fb");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cd7d6b74-0fd3-4fc0-9d5d-61d2154d622d");
+        }
+    }
+}
---
+++ User.Management.API/Models/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace User.Management.API.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
+        private static void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData
+            (
+                new IdentityRole()
+                {
+                    Id = "3b098b51-725c-491c-85c5-e937c8ad6865",
+                    Name = "Admin",
+                    ConcurrencyStamp = "1",
+                    NormalizedName = "Admin"
+                },
+                new IdentityRole()
+                {
+                    Id = "cd7d6b74-0fd3-4fc0-9d5d-61d2154d622d",
+                    Name = "User",
+                    ConcurrencyStamp = "2",
+                    NormalizedName = "User"
+                },
+                new IdentityRole()
+                {
+                    Id = "83f8b40e-19e2-43b1-9d46-0dc0ac9205fb",
+                    Name = "HR",
+                    ConcurrencyStamp = "3",
+                    NormalizedName = "HR"
+                }
+            );
+        }
+
+
+    }
+}
